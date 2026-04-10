76c23dd4a1e25aa32b79df477921ddbf84ffd1ee
Dark Shadow <devops@psquarecompany.com>
2026-04-10 12:45:09 +0530

    resolved the simple issue of CREATED_AT

--- src/utils/googleSheetsUtil.js
+++ src/utils/googleSheetsUtil.js
@@ -171,7 +171,7 @@ async function appendToGoogleSheet(rows) {
       sheet = await doc.addSheet({
         title: sheetTitle,
         headerValues: [
-          'Repository', 'Date', 'Author', 'Email','Commit Time', 'Branch',
+          'CREATED_AT', 'Repository', 'Date', 'Author', 'Email','Commit Time', 'Branch',
           'Commit SHA', 'Commit Link','Commit Message', 'Module Name',
           'Task Type', 'Problem Statement','Ticket ID', 'File Paths',
           'Change Type', 'Total Additions','Total Deletions'
